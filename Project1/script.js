52e88144feb4244924e941649e3be6dfc6fb0623
khawar-javed <82272234+khawar-javed@users.noreply.github.com>
2021-04-12 17:24:11 +0500

    Update script.js
    Start form Fields validation through JS

--- Project1/script.js
+++ Project1/script.js
@@ -4,8 +4,33 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Functions
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className ='form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
 
+function checkFields(inputArray){
+    inputArray.forEach(function(input){
+        if(input.value ===''){
+            showError(input, 'is required');
+        }else{
+            showSuccess(input);
+        }
+    });
+
+}
+
+// EventListener
 form.addEventListener('submit', function(e) {
     e.preventDefault();
+
+    checkFields([username,email,password,password2]);
 
 });
